aeb5899b16dd0147f960436b3f18d4108fb26a9e
Marlon97 <A01379404@itesm.mx>
2020-07-29 01:16:58 -0500

    ejr3

---
+++ seccion8_ejrs/ejr3.py
@@ -0,0 +1,7 @@
+contador = 1
+while(contador<61):
+    print(f"el cuadrado de {contador} es {contador**2}")
+    contador+=1
+
+for contador in range (1,61):
+    print(f"el cuadrado de {contador} es {contador ** 2}")
