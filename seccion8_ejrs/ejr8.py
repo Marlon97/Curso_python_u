3c1e7f460f7e7da27fe3814114be5c37381fcd0e
Marlon97 <A01379404@itesm.mx>
2020-07-29 01:29:37 -0500

    ejr8

--- seccion8_ejrs/ejr8.py
+++ seccion8_ejrs/ejr8.py
@@ -1,1 +1,3 @@
-
+numero1 = int(input("ingresa el numero: "))
+porcentaje = int(input(f"ingresa el procentaje de {numero1}: "))
+print(f"El {porcentaje}% de {numero1} es {numero1*(porcentaje/100)}")
