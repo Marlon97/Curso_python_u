b401125bdbaaf430c1b6fc1f8c3008c108eb49fe
Marlon97 <A01379404@itesm.mx>
2020-07-29 01:19:41 -0500

    ejr4

---
+++ seccion8_ejrs/ejr4.py
@@ -0,0 +1,7 @@
+numero1 = int(input("ingresa el primer numero: "))
+numero2 = int(input("ingresa el segundo numero: "))
+
+print(f"{numero1} + {numero2} = {numero1+numero2}")
+print(f"{numero1} - {numero2} = {numero1-numero2}")
+print(f"{numero1} * {numero2} = {numero1*numero2}")
+print(f"{numero1} / {numero2} = {numero1/numero2}")
