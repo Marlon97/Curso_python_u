13d9db7b9a3b5a0a80640993f0d7a0e0351695d2
Marlon97 <A01379404@itesm.mx>
2020-07-29 01:36:04 -0500

    ejr10

--- seccion8_ejrs/ejr10.py
+++ seccion8_ejrs/ejr10.py
@@ -1,1 +1,7 @@
-
+numero = int(input("ingresa la cantidad de alumnos: "))
+aprobados=0
+for i in range (1,numero+1):
+    if(int(input(f"calificacion del alumno {i}: "))>=70):
+        aprobados+=1
+print(f"alumnos aprobados: {aprobados}")
+print(f"alumnos reprobados: {numero-aprobados}")
