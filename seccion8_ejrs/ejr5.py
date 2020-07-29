b34813ae271e329e529a2e25e4e362849528fbab
Marlon97 <A01379404@itesm.mx>
2020-07-29 01:26:47 -0500

    ejr7

--- seccion8_ejrs/ejr5.py
+++ seccion8_ejrs/ejr5.py
@@ -2,7 +2,7 @@ numero1 = int(input("ingresa el primer numero: "))
 numero2 = int(input("ingresa el segundo numero: "))
 
 if numero2<numero1:
-    print("el numero 1 debe ser mayor al numero 2")
+    print("el numero 1 debe ser menor al numero 2")
 else:
     for i in range(numero1,numero2+1):
         print(i)
